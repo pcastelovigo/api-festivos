7d6c11ea2f90a06d2b5ea3b73fc90d079001f896
madtyn <madtyn@gmail.com>
2024-03-31 01:11:12 +0100

    New: Importa festivos locales y autonomicos de Asturias

--- importar_datos/init.sh
+++ importar_datos/init.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-#GALICIA
+# Descarga de CCAAs
 mkdir -p fuentes/galicia
 wget -nv https://ficheiros-web.xunta.gal/abertos/calendarios/calendario_laboral_2023.xlsx -O fuentes/galicia/2023.xlsx
 wget -nv https://ficheiros-web.xunta.gal/abertos/calendarios/calendario_laboral_2024.xlsx -O fuentes/galicia/2024.xlsx
+curl -k --create-dirs https://sede.asturias.es/documents/217768/291977/Calendario+festivos+Asturias.xlsx/6264daab-9417-f76a-c2f4-116f8f1810f9?t=1658389031223 -o fuentes/asturias/2024.xlsx
+
+# Generacion de ficheros .dat
+python3 asturias.py
+python3 euskadi.py
 python3 galicia.py
+
