a6f003fa4727daceba097f9a624fcf59e608efd5
Pablo Castelo <pablocv@gmx.es>
2024-04-01 10:10:10 +0200

    actualizar fuentes

--- importar_datos/init.sh
+++ importar_datos/init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Descarga de CCAAs
-mkdir -p fuentes/galicia
-wget -nv https://ficheiros-web.xunta.gal/abertos/calendarios/calendario_laboral_2023.xlsx -O fuentes/galicia/2023.xlsx
-wget -nv https://ficheiros-web.xunta.gal/abertos/calendarios/calendario_laboral_2024.xlsx -O fuentes/galicia/2024.xlsx
+curl -k --create-dirs https://ficheiros-web.xunta.gal/abertos/calendarios/calendario_laboral_2023.xlsx -o fuentes/galicia/2023.xlsx
+curl -k --create-dirs https://ficheiros-web.xunta.gal/abertos/calendarios/calendario_laboral_2024.xlsx -o fuentes/galicia/2024.xlsx
 curl -k --create-dirs https://sede.asturias.es/documents/217768/291977/Calendario+festivos+Asturias.xlsx/6264daab-9417-f76a-c2f4-116f8f1810f9?t=1658389031223 -o fuentes/asturias/2024.xlsx
+curl -k --create-dirs https://opendata.euskadi.eus/contenidos/ds_eventos/calendario_laboral_2024/opendata/calendario_laboral_2024.xlsx -o fuentes/euskadi/2024.xlsx
 
 # Generacion de ficheros .dat
 python3 asturias.py
