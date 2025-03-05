646dfc999b5f71c62160cbd587f415a10d57f1ba
Pablo Castelo <pablocv@gmx.es>
2025-03-05 14:46:28 +0000

    euskadi tambien mal

--- importar_datos/init.sh
+++ importar_datos/init.sh
@@ -6,14 +6,10 @@ curl -k --create-dirs https://ficheiros-web.xunta.gal/abertos/calendarios/calend
 curl -k --create-dirs https://ficheiros-web.xunta.gal/abertos/calendarios/calendario_laboral_2024.xlsx -o fuentes/galicia/2024.xlsx
 curl -k --create-dirs https://ficheiros-web.xunta.gal/abertos/calendarios/calendario_laboral_2025.xlsx -o fuentes/galicia/2025.xlsx
 
-curl -k --create-dirs https://opendata.euskadi.eus/contenidos/ds_eventos/calendario_laboral_2024/opendata/calendario_laboral_2024.xlsx -o fuentes/euskadi/2024.xlsx
-curl -k --create-dirs https://opendata.euskadi.eus/contenidos/ds_eventos/calendario_laboral_2025/opendata/calendario_laboral_2025.xlsx -o fuentes/euskadi/2025.xlsx
-
-
 # Generacion de ficheros .dat
 mkdir -p ../datos/
 mkdir -p ../models/
 ln -s -r ./models/holiday.py ../models/holiday.py
-python3 euskadi.py
+
 python3 galicia.py
 
